3d3d4889191365ba7d7048cddfb79e587d7f678f
Kinfe123 <kinfishtech@gmail.com>
2024-12-06 18:11:49 +0300

    fix: making it easier for parser

--- app/constants/components.tsx
+++ app/constants/components.tsx
@@ -27,18 +27,20 @@ export default function LoginPage() {
 `,
         signup_page: `
 import SignUp from "@/components/signup";
-  export const metadata = {
-    title: "SignUp - Better Auth",
-  };
-  export default function SignUpPage() {
-    return (
-      <>
-        <SignUp />
-      </>
-    );
-  }
+export const metadata = {
+  title: "SignUp - Better Auth",
+};
+export default function SignUpPage() {
+  return (
+    <>
+      <SignUp />
+    </>
+  );
+}
 `,
         login: `
+"use client"
+
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -47,74 +49,76 @@ import { signIn } from "@/lib/client";
 import Link from "next/link";
 import clsx from "clsx";
 
-const SimpleUsernameAndPass = () => {
-     const [email, setEmail] = useState("");
-     const [password, setPassword] = useState("");
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-     const handleSubmit = (e) => {
-        e.preventDefault();
-        await signIn.email({
-          email,
-          password
-        });
-     };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    await signIn.email({
+      email,
+      password,
+    });
+  };
 
-     return (
-             <div className="w-full max-w-md rounded-xl dark:bg-background shadow-md ring-1 ring-black/5 dark:transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset]">
-                    <form onSubmit={handleSubmit} className="p-7 sm:p-11">
-                      <div className="flex items-start">
-                        <Link href="/" title="Home">
-                          <h1>Logo</h1>
-                        </Link>
-                      </div>
-                      <h1 className="mt-8 text-base/6 font-medium">Welcome back!</h1>
-                      <p className="mt-1 text-sm/5 dark:text-gray-300 text-gray-600">
-                        Sign in to your account to continue.
-                      </p>
-                      <div className="mt-8 space-y-3">
-                        <Label className="text-sm/5 font-medium">Email</Label>
-                        <Input
-                          required
-                          autoFocus
-                          type="email"
-                          name="email"
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                          className={clsx(
-                            "block w-full rounded-lg border border-gray-200 shadow ring-1 ring-black/10",
-                            "px-[calc(theme(spacing.2)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base/6 sm:text-sm/6",
-                            "data-[focus]:outline data-[focus]:outline-2 data-[focus]:-outline-offset-1 data-[focus]:outline-black"
-                          )}
-                        />
-                      </div>
-                      <div className="mt-8 space-y-3">
-                        <Label className="text-sm/5 font-medium">Password</Label>
-                        <Input
-                          required
-                          type="password"
-                          name="password"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                          className={clsx(
-                            "block w-full rounded-lg border border-gray-200 shadow ring-1 ring-black/10",
-                            "px-[calc(theme(spacing.2)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base/6 sm:text-sm/6",
-                            "data-[focus]:outline data-[focus]:outline-2 data-[focus]:-outline-offset-1 data-[focus]:outline-black"
-                          )}
-                        />
-                      </div>
-                      <div className="mt-8">
-                        <Button type="submit" className="w-full">
-                          Sign in
-                        </Button>
-                      </div>
-                   </form>
-            </div>
-          );
-     };
+  return (
+    <div className="w-full max-w-md rounded-xl dark:bg-background shadow-md ring-1 ring-black/5 dark:transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset]">
+      <form onSubmit={handleSubmit} className="p-7 sm:p-11">
+        <div className="flex items-start">
+          <Link href="/" title="Home">
+            <h1>Logo</h1>
+          </Link>
+        </div>
+        <h1 className="mt-8 text-base/6 font-medium">Welcome back!</h1>
+        <p className="mt-1 text-sm/5 dark:text-gray-300 text-gray-600">
+          Sign in to your account to continue.
+        </p>
+        <div className="mt-8 space-y-3">
+          <Label className="text-sm/5 font-medium">Email</Label>
+          <Input
+            required
+            autoFocus
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className={clsx(
+              "block w-full rounded-lg border border-gray-200 shadow ring-1 ring-black/10",
+              "px-[calc(theme(spacing.2)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base/6 sm:text-sm/6",
+              "data-[focus]:outline data-[focus]:outline-2 data-[focus]:-outline-offset-1 data-[focus]:outline-black",
+            )}
+          />
+        </div>
+        <div className="mt-8 space-y-3">
+          <Label className="text-sm/5 font-medium">Password</Label>
+          <Input
+            required
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className={clsx(
+              "block w-full rounded-lg border border-gray-200 shadow ring-1 ring-black/10",
+              "px-[calc(theme(spacing.2)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base/6 sm:text-sm/6",
+              "data-[focus]:outline data-[focus]:outline-2 data-[focus]:-outline-offset-1 data-[focus]:outline-black",
+            )}
+          />
+        </div>
+        <div className="mt-8">
+          <Button type="submit" className="w-full">
+            Sign in
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
- export default SimpleUsernameAndPass;
+export default SimpleUsernameAndPass;
+
         `,
         signup: `
+
 "use client";
 
 import { Button } from "@/components/ui/button";
@@ -270,7 +274,7 @@ export function SignUp() {
 							await signUp.email({
 								email,
 								password,
-								name: ${fullName},
+								name:\`\${firstName} \${lastName}\`,
 								image: image ? await convertImageToBase64(image) : "",
 								callbackURL: "/dashboard",
 								fetchOptions: {
@@ -317,6 +321,7 @@ async function convertImageToBase64(file: File): Promise<string> {
 		reader.readAsDataURL(file);
 	});
 }
+
             `,
         auth: `
 import {

6f432cd621869e70c754f4d11bc635d6540da440
Kinfe123 <kinfishtech@gmail.com>
2024-12-08 01:51:47 +0300

    fix: url addr

--- meta.ts
+++ meta.ts
@@ -2,5 +2,5 @@ export const metadata = {
   title: "Better Auth UI",
   description:
     "Explore a tuned auth UI that you can copy / paste with better-auth features and capabilities",
-  ogImage: "https;//better-auth.farmui.com/og.png",
+  ogImage: "https://better-auth.farmui.com/og.png",
 };

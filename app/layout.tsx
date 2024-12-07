e3c9d828de42962d91c8bf7fd045afe7a20d5df0
Kinfe123 <kinfishtech@gmail.com>
2024-12-08 00:12:36 +0300

    fix: og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
   title: "Better Auth UI",
   description:
     "Explore a tuned auth UI that you can copy / paste with better-auth features and capabilities",
+  ogImage: "https;//better-auth.farmui.com/og.png",
 };
 
 export default function RootLayout({

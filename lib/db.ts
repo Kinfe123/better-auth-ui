47aa802aef035dd7154f9015942af6c30c5de995
Kinfe123 <kinfishtech@gmail.com>
2025-01-27 13:27:52 +0300

    it happens always

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import "server-only";
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+export let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}

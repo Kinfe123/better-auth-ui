88952c5fb7a1487b4dd360a7a5223e436c6bc3f0
Kinfe123 <kinfishtech@gmail.com>
2025-02-06 00:38:23 +0300

    bottom toolbarr

--- components/codeblock/index.tsx
+++ components/codeblock/index.tsx
@@ -13,7 +13,7 @@ export const CodeSnippet = ({
     <pre className="relative">
       <CodeBlock code={code} language={language} theme={themes.oneDark}>
         <div>
-          <CodeBlock.Code className="bg-transparent !p-4 text-xs rounded-xl dark:shadow-lg whitespace-pre overflow-y-scroll max-h-[70vh]">
+          <CodeBlock.Code className="bg-transparent !p-4 text-xs rounded-xl dark:shadow-lg whitespace-pre overflow-y-scroll h-[70vh] max-h-[70vh]">
             <div className="table-row">
               <CodeBlock.LineNumber className="table-cell pr-4 text-sm text-gray-500 text-right select-none" />
               <CodeBlock.LineContent className="table-cell">

d2bc653c7e5c35ba8d24ed236fcd3baedbe00ced
Kinfe123 <kinfishtech@gmail.com>
2025-08-09 15:23:24 +0300

    ui rvmp

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,14 @@ const BetterAuthUI = () => {
         customPaddings
         id="hero"
       >
-        <div className="container mx-auto">
+        <div className="container relative mx-auto">
           <AuthUIHero />
+          <div className="w-[calc(100%+13rem)] mx-auto -translate-x-1/2 left-1/2 absolute z-20 mb-10">
+            <hr className="border-zinc-800/80" />
+            <div className="relative z-20 h-8 w-[calc(100%-5.8rem)] ml-1 -translate-x-1/2 left-1/2 bg-[repeating-linear-gradient(-45deg,#e0f2fe,#e0f2fe_1px,transparent_1px,transparent_6px)] opacity-[8%]">
+            </div>
+            <hr className="border-zinc-800/80" />
+          </div>
           <ComponentDisplay />
         </div>
       </Section>
--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -60,6 +60,7 @@ export default function AuthUIHero() {
           </Link>
         </div>
       </main>
+      
     </div>
   );
 }

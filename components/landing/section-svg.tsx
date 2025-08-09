b1050ea71b4c01349b65eedd22eac2b057c2cef4
Kinfe123 <kinfishtech@gmail.com>
2025-08-09 14:32:17 +0300

    svg

--- components/landing/section-svg.tsx
+++ components/landing/section-svg.tsx
@@ -1,30 +1,25 @@
-const SectionSvg = ({ crossesOffset }: { crossesOffset: string }) => {
-  return (
-    <>
-      <PlusSvg
-        className={`hidden absolute -top-[0.3125rem] left-[1.5625rem] ${
-          crossesOffset && crossesOffset
-        } pointer-events-none lg:block xl:left-[3.6825rem]`}
-      />
+import { Plus } from "lucide-react";
 
-      <PlusSvg
-        className={`hidden absolute  -top-[0.3125rem] right-[1.4625rem] ${
-          crossesOffset && crossesOffset
-        } pointer-events-none lg:block xl:right-[3.25rem]`}
-      />
-    </>
-  );
+const SectionSvg = ({
+	crossesOffset,
+}: {
+	crossesOffset: string;
+}) => {
+	return (
+		<>
+			<Plus
+				className={`hidden absolute -top-[4.4rem] h-6 w-6 ${
+					crossesOffset && crossesOffset
+				} pointer-events-none lg:block lg:left-[3.275rem] text-neutral-300 dark:text-neutral-600 translate-y-[.5px]`}
+			/>
+
+			<Plus
+				className={`hidden absolute -top-[4.4rem] h-6 w-6 right-[1.4625rem] ${
+					crossesOffset && crossesOffset
+				} pointer-events-none lg:block lg:right-[2.7750rem] text-neutral-300 dark:text-neutral-600 translate-y-[.5px]`}
+			/>
+		</>
+	);
 };
 
 export default SectionSvg;
-
-export const PlusSvg = ({ className = "" }) => {
-  return (
-    <svg className={`${className} || ""`} width="11" height="11" fill="none">
-      <path
-        d="M7 1a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v2a1 1 0 0 1-1 1H1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h2a1 1 0 0 1 1 1v2a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V8a1 1 0 0 1 1-1h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H8a1 1 0 0 1-1-1V1z"
-        fill="#ada8c4"
-      />
-    </svg>
-  );
-};

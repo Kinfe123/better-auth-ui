f3742a348f4e1576c99a60996a5f15e43a006aa4
KinfeMichael Tariku <65047246+Kinfe123@users.noreply.github.com>
2025-10-31 13:02:07 +0300

    ui revamp
    Removed the search button from the navbar component.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -57,18 +57,6 @@ export const Navbar = (/* No props needed here */) => {
               </svg>
             </Link>
           </ul>
-          <Button
-            variant="outline"
-            className="ml-2 flex items-center space-x-2"
-            // to use the setter from the context
-            onClick={() => setOpen(true)}
-          >
-            <Search className="h-4 w-4" />
-            <span>Search</span>
-            <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-              <span className="text-xs">⌘</span>K
-            </kbd>
-          </Button>
           {/* <ThemeToggle /> */}
         </div>
       </nav>
